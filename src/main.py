e6774f9e2454134890158bd7f2e8f0e5c1aac468
Joey Vagedes <joey.vagedes@gmail.com>
2022-04-06 20:52:48 -0700

    Formatting fix

--- src/main.py
+++ src/main.py
@@ -9,12 +9,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Filter(object):
     def __init__(self, level):
         self.__level = level
-    
+
     def filter(self, logRecord):
         return logRecord.levelno <= self.__level
+
 
 class Player(object):
 
@@ -161,16 +163,15 @@ class WhatshouldWePlayBot(discord.Client):
                 f'User starting playing {cur.activity.name}. Added to gamelist')
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setLevel(logging.ERROR)
     stream_handler.addFilter(Filter(logging.ERROR))
     file_handler = logging.FileHandler(f'{date.today()}.log')
 
     logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s",
-        level=logging.INFO,
-        handlers=[stream_handler, file_handler])
-    
+                        level=logging.INFO,
+                        handlers=[stream_handler, file_handler])
+
     client = WhatshouldWePlayBot()
     client.run(os.getenv('TOKEN'))
-
