5da582dee88f8502db18bbbf63231ebeeb60c41c
Zac <zachary.clay.long@gmail.com>
2025-01-30 19:33:16 -0800

    Added some unit tests, made sure that we use sorted() instead of .sort()

--- tests/UnitTests.py
+++ tests/UnitTests.py
@@ -8,13 +8,18 @@ from main import Player
 import discord
 
 load_dotenv()
-TEST_FILE = os.getenv('ID') + '.json'
+TEST_FILE_PLAYER = f"Players\{os.getenv('ID')}.json"
+TEST_FILE_GAMELIST = "GameList.json"
 
-def get_games():
-    with open(TEST_FILE, 'r') as jsonFile:
-        output = json.load(jsonFile)
-        output['games'].sort()
-        return output
+def get_data():
+    with open(TEST_FILE_PLAYER, 'r') as jsonFile:
+        games = json.load(jsonFile)
+        return games
+
+def get_gamelist():
+        with open(TEST_FILE_GAMELIST, 'r') as jsonFile:
+            games = json.load(jsonFile)
+            return games
 
 class TestCommands(unittest.TestCase):
 
@@ -22,14 +27,41 @@ class TestCommands(unittest.TestCase):
         # Adding games
         discord.post(content = "$Add Game1, Game 2")
         time.sleep(1)
-        output = get_games()
-        self.assertEqual(output['games'], ['Game 2','Game1'])  
+        games = get_data()
+        self.assertEqual(sorted(games['games']), sorted(['Game 2','Game1']))  
+
+        # Adding to disallowlist and checking for suggestions
+        discord.post(content = "$disallowlist Game 2")
+        time.sleep(1)
+        games = get_data()
+        self.assertEqual(games['disallowlist'], ['Game 2'])
+
+        # Checking that suggest * works
+        discord.post(content = "$suggest *")
+
+        # Just setting a player count to make sure the functionality works
+        discord.post(content = "$set Game 1, 3")
+        time.sleep(1)
+        game_data = get_gamelist()
+        self.assertEqual(game_data['Game 1'], '3')
+
+        # Making sure suggestion doesn't fail
+        discord.post(content = "$suggest 2")
+
+        # Checking for voice channel suggestions
+        discord.post(content = "$suggest General")
+
+        # Adding to disallowlist and checking for suggestions, should contain help message
+        discord.post(content = "$undisallowlist Game 2")
+        time.sleep(1)
+        games = get_data()
+        self.assertEqual(games['disallowlist'], [])
 
         # Remove a game and test
         discord.post(content = "$remove Game1")
         time.sleep(1)
-        output = get_games()
-        self.assertEqual(output['games'], ['Game 2'])
+        games = get_data()
+        self.assertEqual(games['games'], ['Game 2'])
 
         # Make sure list works with games populated
         discord.post(content = "$list")
@@ -37,8 +69,8 @@ class TestCommands(unittest.TestCase):
         # Remove the last game and make sure it's empty
         discord.post(content = "$REMOVE Game 2")
         time.sleep(1)
-        output = get_games()
-        self.assertEqual(output['games'], [])   
+        games = get_data()
+        self.assertEqual(games['games'], [])   
 
         # Make sure the list doesn't fail when there are no games
         discord.post(content = "$list")
@@ -46,27 +78,51 @@ class TestCommands(unittest.TestCase):
     def test_from_player(self):
         # Adding games
         player.add_games(['Game1', 'Game 2'])
-        output = get_games()
-        print(output)
-        self.assertEqual(output['games'], ['Game 2','Game1'])
+        games = get_data()
+        print(games)
+        self.assertEqual(sorted(games['games']), ['Game 2','Game1'])
 
         # Remove a games and test
         player.remove_games(['Game1'])
-        output = get_games()
-        self.assertEqual(output['games'], ['Game 2'])
+        games = get_data()
+        self.assertEqual(games['games'], ['Game 2'])
 
         player.remove_games(['Game 2'])
-        output = get_games()
-        self.assertEqual(output['games'], [])   
+        games = get_data()
+        self.assertEqual(games['games'], [])   
 
-        # Make sure the get_games method works
-        games = player.get_games()
-        output = get_games()
-        self.assertEqual(output['games'].sort(), games.sort())    
+        # Make sure the get_data method works
+        games = get_data()
+        player_games = player.get_games()
+        self.assertEqual(sorted(games['games']), sorted(player_games)) 
+
+        # Adding games
+        player.add_disallowlist_games(['Game1', 'Game 2'])
+        games = get_data()
+        print(games)
+        self.assertEqual(sorted(games['disallowlist']), sorted(['Game1','Game 2']))
+
+        # Make sure disallow works in get_data
+        player_disallow = player.get_disallowlist()   
+        self.assertEqual(sorted(games['disallowlist']), sorted(player_disallow))
+
+        # Remove a games and test
+        player.remove_disallowlist_games(['Game1'])
+        games = get_data()
+        self.assertEqual(games['disallowlist'], ['Game 2'])
+
+        player.remove_disallowlist_games(['Game 2'])
+        games = get_data()
+        self.assertEqual(games['disallowlist'], [])   
+
+        # Make sure the get_data method works
+        games = get_data()
+        player_games = player.get_games()
+        self.assertEqual(sorted(games['games']), sorted(player_games))   
 
 if __name__ == '__main__':
-    if os.path.exists(TEST_FILE):
-        os.remove(TEST_FILE)
+    if os.path.exists(TEST_FILE_PLAYER):
+        os.remove(TEST_FILE_PLAYER)
 
     user = discord.User
     user.id = os.getenv('ID')
